e61cf541362291d92afb02b4e16bb4ebca8e99cd
Daniil879 <daniilabramov91@gmail.com>
2024-10-26 11:51:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,32 @@ define('db_pass','admin_password');
 // Обявление класса для подключения к бд
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 
-$query = $db->select(true,"*","danil","task_id='1'");
+$query = $db->select(true,"*","danil","");
 
-print_r($query);
  ?>
+ <!DOCTYPE html>
+<html>
+<head>
+    <title>Задачник</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="tasks">
+    <?php
+    for ($i=0; $i < count($query); $i++) { 
+        ?>
+    <div class="task">
+        <div class="task-control">
+            <input type="checkbox" name="">
+        </div>
+        <div class="task-text"><?php echo $query[$i]['task_text'] ?></div>  
+        <div class="task-date">
+            <input type="date" name="">
+        </div>
+    </div>
+        <?php 
+    }
+    ?>
+</div>
+</body>
+</html>
